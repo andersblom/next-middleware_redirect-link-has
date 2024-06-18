74975dcdd468fbdef8ba61f4c34deed448fb3982
Anders Blom <anders@andersblom.dk>
2024-06-18 08:39:27 -0500

    poc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="mb-4">
+          <Link href="/" className="mr-4">Home Link</Link>
+          <a href="/">Home a</a>
+        </div>
+        {children}</body>
     </html>
   );
 }
---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    if (request.nextUrl.pathname === '/foo') {
+        return NextResponse.redirect(new URL('/bar#baz', request.url))
+    }
+}
+
+export const config = {
+    matcher: [
+      /*
+       * Match all request paths except for the ones starting with:
+       * - api (API routes)
+       * - _next/static (static files)
+       * - _next/image (image optimization files)
+       * - favicon.ico (favicon file)
+       */
+      '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    ],
+  }
